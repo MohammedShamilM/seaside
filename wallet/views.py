2fb893a45ba3e2016c36e208b6d3155844af5f54
Mohammed Shamil M <shamilmhd073@gmail.com>
2025-02-21 09:46:32 +0530

    changed all $ sign

--- wallet/views.py
+++ wallet/views.py
@@ -8,8 +8,8 @@ def user_wallet(request):
     User = request.user
     wallet = Wallet.objects.get_or_create(user = User)
     wallet = Wallet.objects.get(user = User)
-    credit_transactions = Transaction.objects.filter(wallet = wallet,transaction_type = 'credit')
-    debit_transactions = Transaction.objects.filter(wallet = wallet,transaction_type = 'debit')
+    credit_transactions = Transaction.objects.filter(wallet = wallet,transaction_type = 'credit').order_by('-created_at')
+    debit_transactions = Transaction.objects.filter(wallet = wallet,transaction_type = 'debit').order_by('-created_at')
 
 
     return render(request,'wallet.html',{'wallet':wallet,'credit_transactions' : credit_transactions,'debit_transactions' : debit_transactions})
