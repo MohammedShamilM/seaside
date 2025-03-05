95aa2442bdbdfd8a797dcd3235537250b23c0d79
Ubuntu <ubuntu@ip-172-31-26-225.eu-north-1.compute.internal>
2025-03-05 04:03:20 +0000

    changes in settings

--- seaside/settings.py
+++ seaside/settings.py
@@ -100,6 +100,32 @@ TEMPLATES = [
 WSGI_APPLICATION = 'seaside.wsgi.application'
 
 
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+	'file': {
+		'level': 'DEBUG',
+		'class': 'logging.FileHandler',
+		'filename': 'debug.log',
+	},
+     },
+    'loggers' : {
+	'django': {
+	    'handlers': ['file'],
+	    'level' : 'DEBUG' ,
+	    'propagate': True,
+	},
+     },
+}
+
+
+
+
+
+
+
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
